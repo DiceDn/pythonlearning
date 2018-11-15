60ad284bff220b26e52047d21f1b7cc44badef31
DiceDn <39012693+DiceDn@users.noreply.github.com>
2018-11-15 23:23:40 +0000

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+!pip install matplotlib
+import matplotlib as mp
+
+import pandas as pd
+
+
+
+
+import sys
+print('\n'.join(sys.path))
+
+Names = ['Pete Davis','Richard Dunn','Jessica Rabbit','Mike Miers']
+print(Names)
